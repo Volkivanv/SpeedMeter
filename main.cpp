0a0cbf5cca5b7ee1c99142742baaa001ee921955
Ivan Volkov <volkivanv@mail.ru>
2021-08-19 23:31:09 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+
+int main() {
+    float speed = 0.0f;
+    float delta;
+
+    do{
+        std::stringstream text;
+        std::cout << "input change of speed!" << std::endl;
+        std::cin >> delta;
+        speed+=delta;
+        if(speed > 150){
+            speed = 150;
+        } else if(speed < 0) {
+            speed = 0;
+        }
+        text << "Car velocity is " << speed << " km/h.";
+        std::cout << text.str() << std::endl;
+
+    }while(abs(speed - 0.0f)>0.01f);
+    std::cout << "Good bye!" << std::endl;
+
+    return 0;
+}
